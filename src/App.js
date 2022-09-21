800a56b46ca319455250e9cf7f503242b63345e7
leximainw <leximainw@gmail.com>
2022-09-21 13:55:09 -0400

    style: remove semicolons for consistency

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-//import logo from './logo.svg';
-import './App.css';
-import Login from './Login';
+//import logo from './logo.svg'
+import './App.css'
+import Login from './Login'
 
 function App() {
     return (
         <Login />
-    );
+    )
 }
 
-export default App;
+export default App
