6568e833cbd96153b71ab4e6c6d47a735ebb7043
leximainw <leximainw@gmail.com>
2022-09-21 14:47:06 -0400

    feat: add owner name to CardDetails

--- src/CardDetails.js
+++ src/CardDetails.js
@@ -2,6 +2,7 @@ const CardDetails = ({card}) => {
     return (
         <div>
             <h1>{card.title}</h1>
+            <div>by {card.owner.username}</div>
             <div>{card.outer_message}</div>
         </div>
     )
