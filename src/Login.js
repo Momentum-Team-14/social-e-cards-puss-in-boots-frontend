bf781b4388bf0e321c323038fd28af3198cf5559
leximainw <leximainw@gmail.com>
2022-09-21 11:17:16 -0400

    feat: handle user registration in Login component

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { handleUpdate } from "./utils";
 
-const Login = ({onSuccess}) => {
+const Login = ({registering, onSuccess}) => {
     const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [confirm, setConfirm] = useState('')
 
     return (   //1qazXSW@  zaq1@WSX
         <form>
@@ -15,6 +17,18 @@ const Login = ({onSuccess}) => {
                     onChange={handleUpdate(setUsername)}
                 />
             </div>
+            {
+                registering ?
+                <div>
+                    <label htmlFor="login-email">Email</label>
+                    <input
+                        type="email"
+                        value={email}
+                        id="login-email"
+                        onChange={handleUpdate(setEmail)}
+                    />
+                </div> : ''
+            }
             <div>
                 <label htmlFor="login-password">Password</label>
                 <input
@@ -24,6 +38,18 @@ const Login = ({onSuccess}) => {
                     onChange={handleUpdate(setPassword)}
                 />
             </div>
+            {
+                registering ?
+                <div>
+                    <label htmlFor="login-confirm">Confirm Password</label>
+                    <input
+                        type="password"
+                        value={confirm}
+                        id="confirm-password"
+                        onChange={handleUpdate(setConfirm)}
+                    />
+                </div> : ''
+            }
         </form>
     )
 }
