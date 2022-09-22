6cd2add577ecaab7df5eeb6ddb336a7347e3d75d
leximainw <leximainw@gmail.com>
2022-09-22 18:08:49 -0400

    feat: add endpoint for fetching all cards

--- src/endpoints.js
+++ src/endpoints.js
@@ -3,6 +3,7 @@ const apiRoot = "https://powerful-island-75819.herokuapp.com/"
 const Urls = {
     login: () => `${apiRoot}/api/auth/token/login`,
     register: () => `${apiRoot}/api/auth/users`,
+    getCards: () => `${apiRoot}/ecards`,
 }
 
 export default Urls
