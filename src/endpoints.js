7de671bcedb4fdde60aba3caadb830248c32d3a4
leximainw <leximainw@gmail.com>
2022-09-22 18:23:54 -0400

    feat: add helper function to get card list from API

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,9 +1,19 @@
+import axios from "axios"
+
 const apiRoot = "https://powerful-island-75819.herokuapp.com"
 
-const Urls = {
+const urls = {
     login: () => `${apiRoot}/api/auth/token/login`,
     register: () => `${apiRoot}/api/auth/users`,
     getCards: () => `${apiRoot}/ecard`,
 }
 
-export default Urls
+const getCards = async () => {
+    const cards = await axios.get(urls.getCards()).then(res => res.data)
+    return cards.map(card => ({...card}))
+}
+
+export {
+    getCards,
+    urls,
+}
