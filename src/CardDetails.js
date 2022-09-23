f9cca6cb745632318b8118f2eb910423186519b0
leximainw <leximainw@gmail.com>
2022-09-23 11:27:42 -0400

    feat: fetch mock card from ID in CardDetails

--- src/CardDetails.js
+++ src/CardDetails.js
@@ -1,4 +1,12 @@
+import { useParams } from "react-router-dom"
+import { cards } from "./mock"
+
 const CardDetails = ({card}) => {
+    const { pk } = useParams()
+    if (!card) {
+        card = cards.filter(card => card.pk === parseInt(pk))[0]
+    }
+
     return (
         <div>
             <h1>{card.title}</h1>
