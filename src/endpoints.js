355a3cfa29a24adb205723f7c2d683587eb270f1
leximainw <leximainw@gmail.com>
2022-09-26 10:52:53 -0400

    fix: make endpoint URLs consistent with backend API

--- src/endpoints.js
+++ src/endpoints.js
@@ -3,9 +3,10 @@ import axios from "axios"
 const apiRoot = "https://powerful-island-75819.herokuapp.com"
 
 const urls = {
-    login: () => `${apiRoot}/api/auth/token/login`,
-    register: () => `${apiRoot}/api/auth/users`,
-    getCards: () => `${apiRoot}/ecard`,
+    login: () => `${apiRoot}/api/auth/token/login/`,
+    register: () => `${apiRoot}/api/auth/users/`,
+    getCards: () => `${apiRoot}/ecard/`,
+    getComments: (pk) => `${apiRoot}/comments/${pk}/`,
 }
 
 const getCards = async () => {
