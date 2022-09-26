66f90416f7ca788f7492abe72207d0d159b6edc6
JeffBuck8 <jdbuckcode@gmail.com>
2022-09-26 12:19:19 -0400

    feat: add CommentList component
    Co-authored-by: Alexis <leximainw@gmail.com>

---
+++ src/CommentList.js
@@ -0,0 +1,20 @@
+import { useNavigate } from "react-router-dom"
+
+const CommentList = ({comments, onSelect=() =>{}}) => {
+    const Comment = ({comment}) =>{
+        return (
+            <article onClick={() => onSelect(comment)}>
+                <div>{comment.owner.username}</div>
+                <div>{comment.text}</div>
+            </article>
+        )
+    }
+
+    return (
+        <div>
+            {comment.map(comment => <Comment comment={comment} key={comment.pk} />)}
+        </div>
+    )
+}
+
+export default CommentList
