ed278684a55cf1c0fe3121d31eb46d07f27ea537
leximainw <leximainw@gmail.com>
2022-09-28 12:58:17 -0400

    feat: implement ProfileNav component

---
+++ src/ProfileNav.js
@@ -0,0 +1,57 @@
+import { useState } from "react"
+import Login from "./Login"
+import { nothing } from "./utils"
+
+const ProfileNav = ({
+    onLogin=nothing,
+    onLogout=nothing,
+    onProfile=nothing,
+    loggedIn=false,
+}) => {
+    const [loginPrompt, setLoginPrompt] = useState(false)
+    const [registering, setRegistering] = useState(false)
+
+    const showLoginPrompt = registering => e => {
+        e.preventDefault()
+        setLoginPrompt(true)
+        setRegistering(registering)
+    }
+
+    return (
+        <div id="profile-nav" style={{
+            display: 'flex',
+            justifyContent: 'end',
+            alignItems: 'center',
+            height: '3rem',
+        }}>
+            { loggedIn ? <>
+                    Logout
+                    Profile
+                </> : <>
+                <div style={{marginRight: '1rem'}}>
+                    <a href="/#" onClick={showLoginPrompt(false)}>Login</a>
+                    &nbsp;or&nbsp;
+                    <a href="/#" onClick={showLoginPrompt(true)}>Register</a>
+                </div>
+                <div style={{
+                    display: loginPrompt ? '' : 'none',
+                    position: 'absolute',
+                    top: '3rem',
+                    right: '0px'
+                }}>
+                    <Login
+                        registering={registering}
+                        onSuccess={
+                            result => {
+                                setLoginPrompt(false)
+                                onLogin(result)
+                            }
+                        }
+                    />
+                </div></>
+            }
+        </div>
+    )
+}
+
+export default ProfileNav
