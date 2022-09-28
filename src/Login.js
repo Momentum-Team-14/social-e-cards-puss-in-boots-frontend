1025004cbf5bb6413011729b2ddb4f9ba3b68b6c
leximainw <leximainw@gmail.com>
2022-09-28 11:24:03 -0400

    feat: add username to Login component callback result
    Co-Authored-By: Jeff Buckley <jdbuckcode@gmail.com>

--- src/Login.js
+++ src/Login.js
@@ -13,7 +13,10 @@ const Login = ({registering, onSuccess=nothing}) => {
     const handleLogin = callback => res => {
         if (res.status === 200) {
             setErrors({})
-            callback(res.data.auth_token)
+            callback({
+                username,
+                token: res.data.auth_token,
+            })
         } else {
             let err = new Error(`Got unexpected response code ${res.status}`)
             err.response = {
